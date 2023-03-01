6d5734006d57718bf9d46e2e4054c78d6040c7cb
motaqabbirRahman <md.motaqabbir.rahman@g.bracu.ac.bd>
2023-03-01 22:59:22 +0600

    userSUbmit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Map Marker Form</title>
+  <!-- Add map library -->
+  <script src="https://maps.googleapis.com/maps/api/js?language=en&key=AIzaSyD93cHVgv78v7---19ZQtimRvVpqi7t_M0"></script>
+  <link rel="stylesheet" href="styles.css" class="style">
+
+  <script>
+    // Initialize map and marker variable
+    var map;
+    var marker;
+
+    function initMap() {
+      // Set default map center
+      var center = { lat: 37.7749, lng: -122.4194 };
+      
+      // Create map object
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: center
+      });
+       
+      // Add click event listener to map
+      map.addListener('click', function(event) {
+        // If marker already exists, remove it from the map
+        if (marker) {
+          marker.setMap(null);
+        }
+        
+        // Create new marker at clicked location
+        marker = new google.maps.Marker({
+          position: event.latLng,
+          map: map,
+          title: 'New marker'
+        });
+
+        // Add marker to map
+  			marker.setMap(map);
+			
+        // Show info window when marker is clicked
+        marker.addListener('click', function() {
+          var contentString = '<div><p>Marker info goes here</p></div>';
+          var infoWindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+          infoWindow.open(map, marker);
+        });
+        
+        // Set form values to clicked location
+        document.getElementById('latitude').value = event.latLng.lat();
+        document.getElementById('longitude').value = event.latLng.lng();
+        
+        // Show form
+        document.getElementById('marker-form').style.display = 'block';
+		document.getElementById('success-msg').style.display = 'none';
+      });
+
+	  
+    }
+  </script>
+</head>
+<body onload="initMap()">
+  <!-- Add map container -->
+  <div id="map" style="height: 500px;"></div>
+  <?php
+		if (isset($_GET['msg'])) {
+		echo "<div id='success-msg'>
+		 Success! Waiting for Approval.
+		 </div>";
+		}
+    ?>
+  <!-- Add marker form -->
+  <div id="marker-form">
+    <form method="post" action="save_marker.php">
+      <input type="hidden" id="latitude" name="latitude" required>
+      <input type="hidden" id="longitude" name="longitude" required>
+
+	  <label for="marker_type" style="color:#003049;">Marker type:</label>
+		<select id="marker_type" name="marker_type">
+			<option value="default">Select Type</option>
+			<option value="🚒">🚒</option>
+			<option value="🧯">🧯</option>
+			<option value="🔥">previous 🔥</option>
+		</select>
+
+      <label for="marker_info" style="color:#003049;">Marker info:</label>
+      <textarea id="marker_info" name="marker_info"></textarea>
+
+      <input type="submit" value="Save marker">
+    </form>
+  </div>
+</body>
+</html>
---
+++ save_marker.php
@@ -0,0 +1,31 @@
+<?php
+// Connect to database
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'fire_map';
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get form data
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+$marker_info = $_POST['marker_info'];
+$marker_type = $_POST['marker_type'];
+
+// Insert data into markers table
+$sql = "INSERT INTO markers (latitude, longitude, marker_info, marker_type) VALUES ($latitude, $longitude, '$marker_info', '$marker_type')";
+if (mysqli_query($conn, $sql)) {
+  $msg = "Marker saved successfully";
+  header("Location: index.php?msg=".urlencode($msg));
+  exit();
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
